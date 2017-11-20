213f05ed3e6d3e8c3ab4e6f20e77bc8d0f4bdb5e
Daniels Kursits <daniels.kursits@gmail.com>
2017-11-20 20:26:44 +0200

    [tokenizer] lifetimed everything

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::rc::Rc;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::iter::{Peekable, Enumerate};
+use std::str::Chars;
 
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Type {
     Number,
     String,
@@ -13,112 +14,77 @@ pub enum Type {
 
     Indent,
     Dedent,
-    Newline,
 
+    EOL,
     EOF,
 }
 
 
 
 #[derive(Debug)]
-pub struct Token {
+pub struct Token<'a> {
     pub ttype: Type,
     pub line: usize,
     pub slice: (usize, usize),
-    pub lexeme: Option<String>,
+    pub lexeme: Option<&'a str>,
 }
 
-impl Token {
+impl<'a> Token<'a> {
     fn new (
         ttype: Type,
         line: usize,
         slice: (usize, usize),
-        lexeme: Option<String>
+        lexeme: Option<&'a str>
     ) -> Token {
         Token { ttype, line, slice, lexeme }
     }
 
-    fn number (line: usize, slice: (usize, usize), lexeme: String) -> Token {
+    fn number (line: usize, slice: (usize, usize), lexeme: &'a str) -> Token<'a> {
         Token::new(Type::Number, line+1, slice, Some(lexeme))
     }
 
-    fn string (line: usize, slice: (usize, usize), lexeme: String) -> Token {
+    fn string (line: usize, slice: (usize, usize), lexeme: &'a str) -> Token<'a> {
         Token::new(Type::String, line+1, slice, Some(lexeme))
     }
 
-    fn word (line: usize, slice: (usize, usize), lexeme: String) -> Token {
+    fn word (line: usize, slice: (usize, usize), lexeme: &'a str) -> Token<'a> {
         Token::new(Type::Word, line+1, slice, Some(lexeme))
     }
 
-    fn symbol (line: usize, slice: (usize, usize), lexeme: String) -> Token {
+    fn symbol (line: usize, slice: (usize, usize), lexeme: &'a str) -> Token<'a> {
         Token::new(Type::Symbol, line+1, slice, Some(lexeme))
     }
 
-    fn indent (line: usize) -> Token {
+    fn indent (line: usize) -> Token<'a> {
         Token::new(Type::Indent, line+1, (0,0), None)
     }
 
-    fn dedent (line: usize) -> Token {
+    fn dedent (line: usize) -> Token<'a> {
         Token::new(Type::Dedent, line+1, (0,0), None)
     }
 
-    fn newline (line: usize) -> Token {
-        Token::new(Type::Newline, line+1, (0,0), None)
+    fn newline (line: usize) -> Token<'a> {
+        Token::new(Type::EOL, line+1, (0,0), None)
     }
 
-    fn eof (line: usize) -> Token {
+    fn eof (line: usize) -> Token<'a> {
         Token::new(Type::EOF, line+1, (0,0), None)
     }
-
 }
 
 
 
-#[derive(Debug)]
-pub struct Alias {
-    name: Token,
-    id: u32,
-}
-
 
 #[derive(Debug)]
-pub struct Segment {
-    name: Token,
-    variant: Rc<Variant>,
-    tokens: Vec<Rc<Token>>,
-}
-
-
-#[derive(Debug)]
-pub struct Variant {
-    name: Token,
-    rule: Rule,
-
-    tokens: Vec<Rc<Token>>,
-    segments: Vec<Segment>,
-    alias: Vec<Alias>,
-
-}
-
-
-#[derive(Debug)]
-pub struct Rule {
-    name: Token,
-    variants: Vec<Variant>,
-}
-
-
-
-#[derive(Debug)]
-pub struct Source {
+pub struct Source<'a> {
     path: String,
     lines: Vec<String>,
-    tokens: Option<Vec<Token>>,
+    tokens: Option<Vec<Token<'a>>>,
     directives: Vec<(String, String)>, // control directives
 }
 
-impl Source {
-    pub fn new (path: &str, ctrl_char: Option<&str>) -> Source {
+impl<'a> Source<'a> {
+    pub fn new (path: &str, ctrl_char: Option<&str>) -> Source<'a> {
         let f: File = match File::open(path) {
             Ok(v) => v,
             Err(_) => panic!("No such file: {}", &path),
@@ -167,20 +133,33 @@ impl Source {
 
 
 
+pub fn matches (iter: &mut Peekable<Enumerate<Chars>>, string: &[String]) -> Option<String> {
+    None
+}
+
+
 pub fn tokenize (src: &mut Source) {
-    let mut indents = vec![0];
+    let mut indents = Vec::new();
     let mut tokens = Vec::new();
 
     for (l, line) in src.lines.iter().enumerate() {
         let mut indent = 0;
         let mut start = false;
+        let mut comment = false;
         let mut iter = line.chars().enumerate().peekable();
 
-        let mut sdelim = Vec::new();
+        let mut string_d = Vec::new();
+        let mut comment_d = Vec::new();
 
         if let Some(string_delim) = src.get_directive("string") {
-            for bit in string_delim.split_whitespace() {
-                sdelim.push(bit.chars().next().unwrap());
+            for delim in string_delim.split_whitespace() {
+                string_d.push(delim.to_string());
+            }
+        }
+
+        if let Some(comment_delim) = src.get_directive("comment") {
+            for delim in comment_delim.split_whitespace() {
+                comment_d.push(delim.to_string());
             }
         }
 
@@ -192,13 +171,13 @@ pub fn tokenize (src: &mut Source) {
             } else if !start {
                 start = true;
 
-                if indent < *indents.last().unwrap() {
+                if indent < *indents.last().unwrap_or(&0) {
                     while indent < *indents.last().unwrap() {
                         tokens.push(Token::dedent(l));
                         indents.pop();
                     }
 
-                } else if indent > *indents.last().unwrap() {
+                } else if indent > *indents.last().unwrap_or(&0) {
                     indents.push(indent);
                     tokens.push(Token::indent(l));
                 }
@@ -207,50 +186,57 @@ pub fn tokenize (src: &mut Source) {
 
             if start {
                 if next.is_numeric() {
-                    while let Some(&(to, next)) = iter.peek() {
+                    while let Some(&(_, next)) = iter.peek() {
                         if !next.is_numeric() {
-                            tokens.push(Token::number(l, (from, to), String::from(&line[from..to])));
                             break;
                         }
-
                         iter.next();
                     }
+                    let to = iter.peek().map(|v| v.0).unwrap_or(line.len());
+                    tokens.push(Token::number(l, (from, to), &line[from..to]));
 
                 } else if next.is_alphabetic() {
-                    while let Some(&(to, next)) = iter.peek() {
+                    while let Some(&(_, next)) = iter.peek() {
                         if !next.is_alphanumeric() {
-                            tokens.push(Token::word(l, (from, to), String::from(&line[from..to])));
+                            break;
+                        }
+                        iter.next();
+                    }
+                    let to = iter.peek().map(|v| v.0).unwrap_or(line.len());
+                    tokens.push(Token::word(l, (from, to), &line[from..to]));
+
+                } else if let Some(delim) = matches(&mut iter.clone(), &string_d) {
+                    let mut last = next;
+
+                    while let Some(&(to, next)) = iter.peek() {
+                        if last != '\\' && matches(&mut iter.clone(), &[delim.clone()]) != None {
+                            tokens.push(Token::string(l, (from+1, to), &line[from+1..to]));
+                            iter = iter.skip(delim.len());
                             break;
                         }
 
+                        last = next;
                         iter.next();
                     }
-
-                } else if let Some(delim) = sdelim.iter().find(|&&c| c == next) {
-                        let mut last = next;
-
-                        while let Some(&(to, next)) = iter.peek() {
-                            if next == *delim && last != '\\' {
-                                tokens.push(Token::string(l, (from+1, to), String::from(&line[from+1..to])));
-                                break;
-                            }
-
-                            last = next;
-                            iter.next();
-                        }
-
-                } else if next == '\n' {
-                    tokens.push(Token::newline(l));
 
                 } else if next.is_whitespace() {
 
                 } else {
-                    tokens.push(Token::symbol(l, (from, from+1), String::from(&line[from..from+1])));
+                    tokens.push(Token::symbol(l, (from, from+1), &line[from..from+1]));
                 }
             }
         }
 
+
+        if tokens.last().map(|t| t.ttype != Type::EOL).unwrap_or(false) {
+            tokens.push(Token::newline(l));
+        }
+
         println!("{:5}| {}", l+1, line);
+    }
+
+    for _ in indents {
+        tokens.push(Token::dedent(src.lines.len()));
     }
     tokens.push(Token::eof(src.lines.len()));
     src.tokens = Some(tokens);
@@ -264,6 +250,6 @@ fn main () {
     tokenize(&mut s);
 
     for token in s.tokens.unwrap() {
-        print!("{:?} ", token.lexeme);
+        println!("{:#?}: {:?} ", token.ttype, token.lexeme);
     }
 }
