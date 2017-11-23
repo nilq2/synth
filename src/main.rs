4c835a04a1473a2c7f114cf44caf303f5841ce61
Daniels Kursits <daniels.kursits@gmail.com>
2017-11-23 22:00:19 +0200

    [main] restructure for multiple input files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
+use std::path::Path;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::sync::Arc;
 
 mod tokenizer;
 mod parser;
@@ -13,35 +15,51 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        let template_f: File = match File::open(&args[1]) {
+    if args.len() == 2 {
+        if !Path::new(&args[1]).exists() {
+            panic!("no such file: {}", &args[1]);
+        }
+        panic!("specify source files after template");
+
+    } else if args.len() > 2 {
+        // template -------------------------------------
+        let t_f: File = match File::open(&args[1]) {
             Ok(v) => v,
             Err(_) => panic!("no such file: {}", &args[1]),
         };
 
-        let template_raw  = BufReader::new(&template_f);
-        let template_lines = template_raw.lines().map(|x| x.unwrap()).collect();
+        let t_raw  = BufReader::new(&t_f);
+        let t_lines = t_raw.lines().map(|x| x.unwrap()).collect();
 
-        let mut template = Source::new(&args[1], Some("//!"), &template_lines);
+        let mut t_src = Source::new(&args[1], Some("//!"), &t_lines);
+        t_src.tokenize();
 
+        let mut t = template::Template::new(t_src);
+        t.parse();
 
-        let unit_f: File = match File::open(&args[2]) {
-            Ok(v) => v,
-            Err(_) => panic!("no such file: {}", &args[2]),
-        };
-
-        let unit_raw  = BufReader::new(&unit_f);
-        let unit_lines = unit_raw.lines().map(|x| x.unwrap()).collect();
-
-        let mut unit = Source::new(&args[2], None, &unit_lines);
-        unit.directives = template.directives.clone();
+        let t_arc = Arc::new(t);
 
 
-        template.tokenize();
-        unit.tokenize();
+        // units ----------------------------------------
+        let mut unit_iter = args.iter();
+        unit_iter.nth(1);
 
-        //let parser = Parser::new();
+        for unit in unit_iter {
+            let u_f: File = match File::open(&unit) {
+                Ok(v) => v,
+                Err(_) => panic!("no such file: {}", &unit),
+            };
 
+            let u_raw  = BufReader::new(&u_f);
+            let u_lines = u_raw.lines().map(|x| x.unwrap()).collect();
+
+            let mut u_src = Source::new(unit, None, &u_lines);
+            u_src.directives = t_arc.source.directives.clone();
+            u_src.tokenize();
+
+            let mut u = unit::Unit::new(u_src, t_arc.clone());
+            u.parse();
+        }
 
     } else {
         println!("source file not supplied: synth <file>")
