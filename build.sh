46174e5f94822025584143a63185f319f6f27a78
Daniels Kursits <daniels.kursits@gmail.com>
2017-11-23 22:00:52 +0200

    [build] moved tests to their own folder

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ while inotifywait -q -r -e close_write ./ ../examples/
 do
    clear
    clear
-   RUST_BACKTRACE=1 cargo run testing.t basics.pi
+   RUST_BACKTRACE=1 cargo run tests/test.t tests/unit1.pi tests/unit2.pi
 done
